f541781231eccb2e723ba4424d3908cfbdf902f2
Devin <studyzy@gmail.com>
2019-04-20 00:50:51 +0800

    Fix some UT errors

--- contracts/Hello/Hello.go
+++ contracts/Hello/Hello.go
@@ -10,7 +10,7 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ontio/ontology-/core/types"
+	"github.com/ethereum/go-ethereum/core/types"
 )
 
 // HelloABI is the input ABI used to generate the binding from.
